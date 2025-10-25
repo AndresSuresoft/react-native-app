fdb34d1e3ab3445728de149ccfe08cb8c0188785
AndresSuresoft <andres.gaona@assuresoft.tech>
2025-10-25 08:44:10 -0300

    Movie DA Project

--- App.tsx
+++ App.tsx
@@ -1,11 +1,32 @@
-import { SafeAreaProvider, useSafeAreaInsets, } from 'react-native-safe-area-context';
-import Home from './src/screens/Home/Home'
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import MyStackNavigator from './src/navigation/MyStackNavigator.tsx';
+import { WishlistProvider } from './src/contexts/Wishlist/WishlistProvider';
+import { MovieModalProvider } from './src/contexts/MovieModal/MovieModalProvider';
+import { useTheme } from './src/contexts/Theme/ThemeContext.tsx';
+import { getNavigationTheme } from './src/navigation/theme.ts';
+import { useMemo } from 'react';
+
 
 function App() {
+  const { themeMode } = useTheme();
+   const navTheme = useMemo(() => {
+    return getNavigationTheme(themeMode);
+  }, [themeMode]);
+  
   return (
-    <SafeAreaProvider>
-      <Home />
-    </SafeAreaProvider>
+    <GestureHandlerRootView>
+        <SafeAreaProvider>
+          <WishlistProvider>
+            <MovieModalProvider>
+              <NavigationContainer theme={navTheme}>
+                <MyStackNavigator />
+              </NavigationContainer>
+            </MovieModalProvider>
+          </WishlistProvider>
+        </SafeAreaProvider>
+    </GestureHandlerRootView>
   );
 }
 
--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,15 @@
+const path = require('path');
+
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
   plugins: [
-  [
-  'module:react-native-dotenv',
-  {
-  moduleName: '@env',
-  path: '.env',
-  },
-  ],
+    [
+      'module:react-native-dotenv',
+      {
+        moduleName: '@env',
+        path: path.resolve(__dirname, '.env'),
+      },
+    ],
+    'react-native-worklets/plugin',
   ],
 };
